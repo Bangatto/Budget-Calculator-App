e0379de3ec0978b92464e7d82d7e9ef507156699
Gattuoch Kuon <kuon2018@outlook.com>
2020-07-01 16:20:25 -0600

    Added an image

---
+++ src/shared/model/budget-item-model.ts
@@ -0,0 +1,6 @@
+export class BudgetItem{
+    //you can also define the two properties here and declare them in the constructor.
+    constructor(public description: string, public amount: number){
+        
+    }
+}
